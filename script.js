0beac85bb59c0791033cb52c49c46175dc929896
Burntmyledger <lucasqumen2003@gmail.com>
2025-08-08 20:13:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ document.querySelectorAll('.product-card').forEach(card => {
         const rect = card.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const rotateX = ((y - centerY) / centerY) * 10;
-        const rotateY = ((x - centerX) / centerX) * 10;
-        card.style.transform = `perspective(500px) rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+        card.style.transform = `scale(1.05) translate(${(x - rect.width / 2) / 10}px, ${(y - rect.height / 2) / 10}px)`;
     });
     card.addEventListener('mouseleave', () => {
-        card.style.transform = 'perspective(500px) rotateX(0) rotateY(0) scale(1)';
+        card.style.transform = 'scale(1)';
     });
 });
